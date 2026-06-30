f9aa787750b4c963bb66bd9618547d3df8a37217
Pedro Facundo <pfacs@hotmail.com>
2026-06-30 18:59:51 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -3,6 +3,24 @@
 
 // Structs
 
+typedef struct {
+	int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct {
+	char nome[100];
+    Data data_nascimento;
+} Autor;
+
+typedef struct {
+	int id;
+    char titulo[100];
+    Autor autor;
+    Data data_escrita;
+} LivroMagico;
+
 // Protótipos das funções
 
 #endif
